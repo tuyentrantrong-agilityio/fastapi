413eedfc02831ce805ccee86e6a47cc2c0369618
Tuyen Tran <tuyen.trantrong@asnet.com.vn>
2025-12-19 17:23:15 +0700

    setting example project for python

---
+++ my_python_project/main.py
@@ -0,0 +1,28 @@
+def main():
+    print("Hello from my-python-project!")
+
+
+if __name__ == "__main__":
+    main()
+# commment in
+
+print(" my name is")
+for i in range(5):
+    print(" tuyen five times" + str(i))
+
+a = not True
+if a:
+    print(" a is true")
+
+42 == 42
+spam = 0
+while spam < 5:
+    print("i learn python " + str(spam))
+    spam += 1
+
+chuoi = "hello worldthis is \na long stringthat spans multiple lines"
+result = chuoi * 5
+print(result)
+
+test = 'abcdef'
+print(len(test))
