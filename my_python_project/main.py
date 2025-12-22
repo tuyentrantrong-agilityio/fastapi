3ded4e56466670dfc011b7fe0aa70f46ef66ceb4
Tuyen Tran <tuyen.trantrong@asnet.com.vn>
2025-12-22 17:42:17 +0700

    example about function, method of string

--- my_python_project/main.py
+++ my_python_project/main.py
@@ -24,5 +24,63 @@ chuoi = "hello worldthis is \na long stringthat spans multiple lines"
 result = chuoi * 5
 print(result)
 
-test = 'abcdef'
+test = "abcdef"
 print(len(test))
+
+from turtle import penup, pendown, forward
+
+
+def jump(lenght):
+    """Move forward length units without leaving a trail.
+
+    Postcondition: Leaves the pen down.
+    """
+    penup()
+    forward(lenght)
+    pendown()
+
+
+import random
+
+random.randint(1, 46)
+
+
+def team(text, age):
+    print(text)
+    print(age)
+
+
+team("hello", 86)
+team("world", 96)
+team("at", 36)
+team("vietnam", "fgsdfg")
+
+
+def team(text, age=0, success=False):
+    print(text)
+    print(age)
+    print(success)
+
+
+team("paramater defualt", 861)
+
+
+def kteam(name, member):
+    print(name)
+    print(member)
+
+
+a = "how method of string concatenation works"
+# b = a.capitalize()
+# b = a.upper()
+# b = a.lower()
+# b = a.swapcase()
+b = a.title()
+b = a.center(50,'-')
+b = a.ljust(50,'-')
+b = a.rjust(50,'-')
+c = 'có gì hót'
+d = c.encode('utf-8')
+e = c.join(['a', 'b', 'c','d'])
+d = a.strip() # xóa hết khoảng trắng giống strim của dart
+print(e)
