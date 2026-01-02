1978b8f77ab66ced1d4cbd7b683ecc7e5400683c
Tuyen Tran <tuyen.trantrong@asnet.com.vn>
2026-01-02 17:17:59 +0700

    feat: add hello CLI program (chapter 1)

---
+++ my_python_project/chapter_1/hello01_print.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# This program prints Hello, World!
+print("Hello World!")
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ my_python_project/chapter_1/hello03_shebang.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# This program prints Hello, World!
+print("Hello World!")
